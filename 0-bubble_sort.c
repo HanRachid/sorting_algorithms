a2aaa645e2018121fe2277f5b4206a2034d5aa06
HanRachid <handaouirachid@gmail.com>
2023-07-25 21:10:47 +0100

    fix betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,26 +2,32 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * bubble_sort - sorts array with bubble sort algorithm
+ *
+ * @arr: array to be sorted
+ * @size: size of array
+ */
 void bubble_sort(int arr[], size_t size)
 {
 
-    int i, j;
-    int temp;
+	int i, j;
+	int temp;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - i; j++)
-        {
-            if (j + 1 < size)
-            {
-                if (arr[j] > arr[j + 1])
-                {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    print_array(arr, size);
-                }
-            }
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			if (j + 1 < size)
+			{
+				if (arr[j] > arr[j + 1])
+				{
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					print_array(arr, size);
+				}
+			}
+		}
+	}
 }
